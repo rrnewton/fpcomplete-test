d2d4e491d6956d66bd86aa3087a7778fc99c8278
Ryan Newton <rrnewton@indiana.edu>
2013-07-29 14:04:37 +0000

    initial commit

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- | Main entry point to the application.
 
 module Main where
+
+import Data.Array.Accelerate
 
 -- | The main entry point.
 main :: IO ()
 main = do
   putStrLn "Welcome to the FP Haskell Center!"
+  putStrLn  "hi "  
+  let x = 333
+      yy = "hmm"
+  return ()
+
+
+
+-- there-- there-- there-- there
+
