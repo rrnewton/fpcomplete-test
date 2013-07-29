29a0cb3749ddf0805671d5120e62018f6b6ffacf
Ryan Newton <rrnewton@indiana.edu>
2013-07-29 14:17:56 +0000

    messing around

--- src/Main.hs
+++ src/Main.hs
@@ -5,17 +5,24 @@
 module Main where
 
 import Data.Array.Accelerate
+import qualified Data.ByteString.Char8 as B
+
+-- | This is a haddock for test.  Ok, I guess those don't get into the ctrl-I docs.
+test = 39
+
+-- | This is a haddock for bar.
+bar :: Int
+bar = test + 3
 
 -- | The main entry point.
 main :: IO ()
 main = do
   putStrLn "Welcome to the FP Haskell Center!"
-  putStrLn  "hi "  
-  let x = 333
+  B.putStrLn  "hi "  
+  let x = 333 :: Int
       yy = "hmm"
+  B.putStrLn yy 
+  -- putStrLn yy
   return ()
 
-
-
 -- there-- there-- there-- there
-
---
+++ src/MyMod.hs
@@ -0,0 +1,10 @@
+
+-- | here are haddocs for MyMod
+module MyMod where
+
+import Data.Word
+
+-- | Here are haddocs for mydef.
+mydef :: Word64
+mydef = 1988383
+
